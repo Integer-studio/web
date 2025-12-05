f5403012752d577ba45a70ba2c5c402b4f0ac1b3
Antonín Kučera <81852696+Limit-sest@users.noreply.github.com>
2025-12-05 22:41:30 +0100

    Add content config

---
+++ src/pages/content.config.ts
@@ -0,0 +1,26 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const projects = defineCollection({
+  loader: glob({ pattern: '**/*.md', base: './src/pages/projects' }),
+  schema: ({ image }) =>
+    z.object({
+      name: z.string(),
+      tags: z.array(z.string()),
+      year: z.string(),
+      description: z.string(),
+      image: image(),
+      priority: z.number(),
+      featured: z.boolean().optional(),
+      links: z
+        .array(
+          z.object({
+            name: z.string(),
+            url: z.string(),
+          })
+        )
+        .optional(),
+    }),
+});
+
+export const collections = { projects };
